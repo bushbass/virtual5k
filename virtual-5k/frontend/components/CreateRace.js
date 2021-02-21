e2ab018a54f490981c5eb8aadae3676947acec54
bushbass <alex.nielsen@gmail.com>
2021-02-21 10:50:49 -0500

    fixed and finished 23

--- virtual-5k/frontend/components/CreateRace.js
+++ virtual-5k/frontend/components/CreateRace.js
@@ -1,39 +1,67 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateRace() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'nice race',
     price: 2345,
     description: 'a greate reace for emvyone',
   });
   return (
     <div>
-      <form action="">
-        <label htmlFor="name">
-          name
-          <input
-            onChange={handleChange}
-            value={inputs.name}
-            type="text"
-            name="name"
-            id="name"
-            placeholder="name"
-          />
-        </label>{' '}
-        <label htmlFor="price">
-          name
-          <input
-            onChange={handleChange}
-            value={inputs.price}
-            type="number"
-            name="price"
-            id="price"
-            placeholder="price"
-          />
-        </label>
-      </form>
-      <button onClick={clearForm}>clear form </button>
-      <button onClick={resetForm}>reset form </button>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(inputs);
+        }}
+      >
+        <fieldset>
+          <label htmlFor="image">
+            image
+            <input
+              required
+              onChange={handleChange}
+              type="file"
+              name="image"
+              id="image"
+            />
+          </label>
+          <label htmlFor="name">
+            name
+            <input
+              onChange={handleChange}
+              value={inputs.name}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+            />
+          </label>
+          <label htmlFor="price">
+            name
+            <input
+              onChange={handleChange}
+              value={inputs.price}
+              type="number"
+              name="price"
+              id="price"
+              placeholder="price"
+            />
+          </label>{' '}
+          <label htmlFor="description">
+            Description
+            <textarea
+              onChange={handleChange}
+              value={inputs.description}
+              name="description"
+              id="description"
+              placeholder="description"
+            />
+          </label>
+          <button type="submit">+ Add Race</button>
+        </fieldset>
+      </Form>
     </div>
   );
 }
